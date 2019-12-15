9f7ce5a10f438fa9afcfe6eddb00f8448e163449
joebadmus <joebadmus@gmail.com>
2019-12-15 06:25:35 +0000

    Added new branch and s3backend

---
+++ s3backend.tf
@@ -0,0 +1,43 @@
+provider "aws" {
+  region = "eu-west-2"
+}
+
+resource "aws_s3_bucket" "java-infra-dev-backend" {
+  bucket = "joebadmus-dev-s3-backend"
+
+  versioning {
+    enabled = true
+  }
+
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
+# terraform {
+#   backend "s3" {
+#     bucket = "joebadmus-dev-s3-backend"
+#     key    = "infra/lock/terraform.tfstate"
+#     # dynamodb_table = "terraform-state-lock-dynamo"
+#     region = "eu-west-2"
+#   }
+# }
+
+
+
+
+# resource "aws_dynamodb_table" "dynamodb-terraform-state-lock" {
+#   name           = "terraform-state-lock-dynamo"
+#   hash_key       = "LockID"
+#   read_capacity  = 20
+#   write_capacity = 20
+
+#   attribute {
+#     name = "LockID"
+#     type = "S"
+#   }
+
+#   tags {
+#     Name = "DynamoDB Terraform State Lock Table"
+#   }
+# }
