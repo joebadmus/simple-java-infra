8479ad16a8cff585ec55ce2b0f81b495fe7ae9cc
joebadmus <joebadmus@gmail.com>
2019-12-16 04:07:22 +0000

    Added lock file

--- s3backend.tf
+++ s3backend.tf
@@ -12,32 +12,36 @@ resource "aws_s3_bucket" "java-infra-dev-backend" {
   lifecycle {
     prevent_destroy = true
   }
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
 }
 
-# terraform {
-#   backend "s3" {
-#     bucket = "joebadmus-dev-s3-backend"
-#     key    = "infra/lock/terraform.tfstate"
-#     # dynamodb_table = "terraform-state-lock-dynamo"
-#     region = "eu-west-2"
-#   }
-# }
+terraform {
+  backend "s3" {
+    bucket = "joebadmus-dev-s3-backend"
+    key    = "infra/lock/terraform.tfstate"
+    dynamodb_table = "terraform-state-lock-dynamo"
+    region = "eu-west-2"
+  }
+}
 
 
+resource "aws_dynamodb_table" "dynamodb-terraform-state-lock" {
+  name           = "terraform-state-lock-dynamo"
+  hash_key       = "LockID"
+  read_capacity  = 20
+  write_capacity = 20
 
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
 
-# resource "aws_dynamodb_table" "dynamodb-terraform-state-lock" {
-#   name           = "terraform-state-lock-dynamo"
-#   hash_key       = "LockID"
-#   read_capacity  = 20
-#   write_capacity = 20
-
-#   attribute {
-#     name = "LockID"
-#     type = "S"
-#   }
-
-#   tags {
-#     Name = "DynamoDB Terraform State Lock Table"
-#   }
-# }
+  tags = {
+    Name = "DynamoDB Terraform State Lock Table"
+    Environment = "dev"
+  }
+}
