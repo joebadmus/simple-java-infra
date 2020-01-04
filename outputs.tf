5ec6218afb5ed0b56a0c6c2130661f12175e05b6
joebadmus <joebadmus@gmail.com>
2020-01-04 05:37:12 +0000

    Removed redundant files

--- outputs.tf
+++ outputs.tf
@@ -10,9 +10,6 @@ output "Private_Subnets" {
   value = aws_subnet.private_subnets.*.id
 }
 
-# output "Bastion_DNS" {
-#   value = "${aws_route53_record.bastion.name}"
-# }
 output "Jumpbox_SG_ID" {
   value = "${aws_security_group.jumpbox-sg.id}"
 }
